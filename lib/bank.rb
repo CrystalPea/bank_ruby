c479472b5259f4b5fa8ec98b81ae29b7a2cdf382
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-23 10:29:10 +0000

    Bank class responds to balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,4 @@
 class Bank
+  attr_reader :balance
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,5 +1,8 @@
 require 'bank'
 
 describe Bank do
+  subject(:bank) { described_class.new }
+
+  it { is_expected.to respond_to :balance }
 
 end
