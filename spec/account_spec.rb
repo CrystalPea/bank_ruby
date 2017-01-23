557ac698f3d7315cf457d799675bea416e20ef19
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-23 11:02:26 +0000

    Changed class name from bank to account to better reflect responsibility

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,24 +1,24 @@
-require 'bank'
+require 'account'
 
-describe Bank do
-  subject(:bank) { described_class.new }
+describe Account do
+  subject(:account) { described_class.new }
 
   context "making deposits and withdrawals" do
     before do
-      bank.make_deposit(1000)
+      account.make_deposit(1000)
     end
-    
+
     describe "#make_deposit" do
       it "adds deposit to the balance" do
-        expect(bank.balance).to eq 1000
+        expect(account.balance).to eq 1000
       end
 
     end
 
     describe "#make_withdrawal" do
       it "substracts deposit from the balance" do
-        bank.make_withdrawal(300)
-        expect(bank.balance).to eq 700
+        account.make_withdrawal(300)
+        expect(account.balance).to eq 700
       end
 
     end
