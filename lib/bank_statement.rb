02262ef06ae6d8c764e3f45f05fcc3f2bd7097d3
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-23 14:39:42 +0000

    Bank Statement instance prints out the statement

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,3 +1,5 @@
+require "operation"
+
 class BankStatement
   attr_reader :history
 
@@ -7,6 +9,18 @@ class BankStatement
 
   def add_operation(operation)
     self.history << operation
+  end
+
+  def prepare_statement
+    statement = "date       || credit || debit  || balance\n"
+    history.each do |operation|
+      statement += "#{operation.date.strftime("%Y/%m/%d")} || #{operation.type == :credit ? operation.amount : "       "}|| #{operation.type == :debit ? operation.amount : "       "} || #{operation.balance}\n"
+    end
+    statement
+  end
+
+  def print_statement
+    print prepare_statement
   end
 
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -1,8 +1,10 @@
-require 'bank_Statement'
+require 'bank_statement'
+require 'date'
 
 describe BankStatement do
   subject(:statement) { described_class.new }
-  let(:operation) { double :operation }
+  let(:operation) { double :operation, date: Date.new(2017,1,22), amount: 1000.00, type: :credit, balance: 1000.00 }
+  let(:operation_2) {double :operation_2, date: Date.new(2017,1,23), amount: 300.00, type: :debit, balance: 700.00 }
 
   it "has empty history on initialisation" do
     expect(statement.history).to eq []
@@ -11,5 +13,11 @@ describe BankStatement do
   it "can receive operations and add them to the history" do
     statement.add_operation(operation)
     expect(statement.history).to eq [operation]
+  end
+
+  it "prints operations in reverse chronological order" do
+    statement.add_operation(operation)
+    statement.add_operation(operation_2)
+    expect{statement.print_statement}.to output("date       || credit || debit  || balance\n23/01/2017 ||        || 300.00 || 700.00\n22/01/2017 || 1000.00||        || 1000.00").to_stdout
   end
 end
