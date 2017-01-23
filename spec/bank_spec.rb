c864dbfe2bf9ae6c52a3c0989fdecac86b9a4d1f
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-23 10:41:19 +0000

    Deleted redundant tests

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,11 +3,7 @@ require 'bank'
 describe Bank do
   subject(:bank) { described_class.new }
 
-  it { is_expected.to respond_to :balance }
-
-  it { is_expected.to respond_to(:make_deposit).with(1).argument }
-
-  it "making deposit affects the balance" do
+  it "making deposit registers in the balance" do
     bank.make_deposit(1000)
     expect(bank.balance).to eq 1000
   end
